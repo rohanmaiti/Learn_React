793071303384ab4560800ac5e8e114aff206404c
Rohan Maiti <maitirohan2019@gmail.com>
2024-07-14 23:37:26 +0530

    rendering list using class props

--- src/components/02_class.js
+++ src/components/02_class.js
@@ -3,7 +3,10 @@ class Class_02 extends Component{
     render(){
         // rendering list using JSX
         return (<>
-        
+        {
+            this.props.array.map(ele=>{<li>{ele}</li>})
+        }
         </>)
     }
 }
+export default Class_02;
